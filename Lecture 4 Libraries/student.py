e76786405ef2f6c6cabb3b5908d9792f1f778938
Harshvyas2704 <harshvyasdev@gmail.com>
2023-03-29 11:47:37 +0530

    Style with Black

---
+++ Lecture 4 Libraries/student.py
@@ -0,0 +1,12 @@
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw"
+}
+for student in students:
+    print(student)
+
+# Styling -> Black
+# To run use -> black student.py

f0fac5823ef98e0ee3fb8d5269a567346630a6af
Harshvyas2704 <harshvyasdev@gmail.com>
2023-03-30 13:49:55 +0530

    RE pending

--- Lecture 7 Regular Expression/validate.py
+++ Lecture 7 Regular Expression/validate.py
@@ -1,1 +1,19 @@
+import re
+
 email = input("What's your email? ").strip()
+
+if re.search(".*@.*", email):
+    print("Valid")
+else:
+    print("Invalid")
+
+
+"""
+. any character except a newline
+* 0 or more repetitions
++ 1 or more repetitions
+? 0 or 1 repititions
+{m} m repititions
+{m, n} m - n repititions
+
+"""
